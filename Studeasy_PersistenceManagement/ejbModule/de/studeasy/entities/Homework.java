0670e4ac3ca7a78c7714439c75cbc5f8dba7e87a
user <tobiasriegel@t-online.de>
2015-06-04 16:03:31 +0200

    angefangen mit PersistenceManagement

---
+++ Studeasy_PersistenceManagement/ejbModule/de/studeasy/entities/Homework.java
@@ -0,0 +1,40 @@
+package de.studeasy.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+import de.studeasy.common.*;
+
+@Entity
+public class Homework implements Serializable, IHomework {
+
+	private static final long serialVersionUID = 9153525651671974891L;
+	
+	@Id
+	@GeneratedValue
+	private int homeworkID;
+	private String description;
+	
+	@OneToMany(cascade=CascadeType.PERSIST)
+	private ILesson lesson;
+	
+	public int getHomeworkID() {
+		return homeworkID;
+	}
+	public void setHomeworkID(int homeworkID) {
+		this.homeworkID = homeworkID;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public ILesson getLesson() {
+		return lesson;
+	}
+	public void setLesson(ILesson lesson) {
+		this.lesson = lesson;
+	}
+}
