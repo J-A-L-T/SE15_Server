4373e473672865ad40c38f544cbb5ea152adf183
Sir2late <mc.2late@gmail.com>
2015-06-18 08:16:55 +0200

    neue Pupil hinzugefügt

--- Studeasy_PersistenceManagement/ejbModule/de/studeasy/dao/DataBuilder.java
+++ Studeasy_PersistenceManagement/ejbModule/de/studeasy/dao/DataBuilder.java
@@ -26,8 +26,9 @@ import de.studeasy.systeminterfaces.ISubject;
 import de.studeasy.systeminterfaces.ITeacher;
 
 /**
- * Der DataBuilder ist eine Singelton Session Bean und wird bei dem Start des Servers aufgerufen,
- * um Testdaten zu erzeugen.
+ * Der DataBuilder ist eine Singelton Session Bean und wird bei dem Start des
+ * Servers aufgerufen, um Testdaten zu erzeugen.
+ * 
  * @author Tobias Riegel
  *
  */
@@ -38,25 +39,38 @@ public class DataBuilder {
 	private static final Logger logger = Logger.getLogger(DataBuilder.class);
 
 	private static int CourseID;
-	
+
 	@PersistenceContext
 	private EntityManager em;
-	
+
 	@PostConstruct
 	private void init() {
 		ICourse klasse = em.find(Course.class, CourseID);
 		if(klasse==null) {
 			//Course noch nicht vorhanden, also neu anlegen mit Schülern, Lehrer, Unterricht, Fach und Hausaufgabe
 			ITeacher albus = new Teacher("Dumbledore", "Albus", "test", 'm', new Course());
-			klasse = new Course(7, 'a', albus);
+			klasse = new Course(1, 'a', albus);
 			this.CourseID = klasse.getCourseID();
 			albus.setCourse(klasse);			
 			
 			IPupil harry = new Pupil("Potter","Harry", "Hedwig", 'm', klasse);
-			IPupil ron = new Pupil("Wesley", "Ron", "Hermine", 'w', klasse);
+			IPupil ron = new Pupil("Wesley", "Ron", "RW", 'm', klasse);
+			IPupil lavander = new Pupil("Brown", "Lavender", "LB", 'w', klasse);
+			IPupil hermine = new Pupil( "Granger", "Hermine","HG", 'w', klasse);
+			IPupil parvati = new Pupil("Patil", "Parvati", "PP", 'm', klasse);
+			IPupil seamus = new Pupil("Finnigan", "Seamus", "SF", 'm', klasse);
+			IPupil neville = new Pupil("Longbottom", "Neville", "NL", 'm', klasse);
+			IPupil dean = new Pupil("Thomas", "Dean", "DT", 'm', klasse);
+			
 			klasse.addNewPupil(harry);
 			klasse.addNewPupil(ron);
-			
+			klasse.addNewPupil(lavander);
+			klasse.addNewPupil(hermine);
+			klasse.addNewPupil(parvati);
+			klasse.addNewPupil(seamus);
+			klasse.addNewPupil(neville);
+			klasse.addNewPupil(dean);		
+					
 			IRoom room1 = new Room("D101");
 			IRoom room2 = new Room("D102");
 			
@@ -80,6 +94,18 @@ public class DataBuilder {
 			logger.info("Neu angelegt " + harry);
 			em.persist(ron);
 			logger.info("Neu angelegt " + ron);
+			em.persist(lavander);
+			logger.info("Neu angelegt " + lavander);
+			em.persist(hermine);
+			logger.info("Neu angelegt " + hermine);
+			em.persist(parvati);
+			logger.info("Neu angelegt " + parvati);
+			em.persist(seamus);
+			logger.info("Neu angelegt " + seamus);
+			em.persist(neville);
+			logger.info("Neu angelegt " + neville);
+			em.persist(dean);
+			logger.info("Neu angelegt " + dean);
 			em.persist(room1);
 			logger.info("Neu angelegt " + room1);
 			em.persist(room2);
